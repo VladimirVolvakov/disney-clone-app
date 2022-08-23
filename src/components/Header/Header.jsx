45883cced4800f2f2a9ef4455e8e6b3c68092ba6
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-24 02:20:04 +0300

    feat: add Header component markup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
 // Styles:
-import { NavigationBar } from "./Header.styles";
+import { Logo, NavigationBar, NavigationMenu, UserImage } from "./Header.styles";
 
 const Header = () => {
   return (
     <NavigationBar>
-
+      <Logo src="/images/logo.svg" />
+      <NavigationMenu>
+        <a href="#">
+          <img src="/images/home-icon.svg" alt="Home" />
+          <span>HOME</span>
+        </a>
+        <a href="#">
+          <img src="/images/search-icon.svg" alt="Search" />
+          <span>SEARCH</span>
+        </a>
+        <a href="#">
+          <img src="/images/watchlist-icon.svg" alt="Watch List" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="#">
+          <img src="/images/original-icon.svg" alt="Originals" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="#">
+          <img src="/images/movie-icon.svg" alt="Movies" />
+          <span>MOVIES</span>
+        </a>
+        <a href="#">
+          <img src="/images/series-icon.svg" alt="Series" />
+          <span>SERIES</span>
+        </a>
+      </NavigationMenu>
+      <UserImage src="/images/no-userphoto.jpg" alt="User" />
     </NavigationBar>
   );
 };
---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const NavigationBar = styled.nav``;
+
+export const Logo = styled.img``;
+
+export const NavigationMenu = styled.div``;
+
+export const UserImage = styled.img``;
