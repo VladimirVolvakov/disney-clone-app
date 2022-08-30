68dabfee0432e9aed9a976c33097789d84d1b71e
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-30 09:41:15 +0300

    feat: install react-router-dom & add basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 // Styles:
-import './App.css';
+import "./App.css";
 // Components:
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+// Router:
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/details" element={<MovieDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,10 @@
+// Styles:
+import { Container } from "./MovieDetails.styles";
+
+const MovieDetails = () => {
+  return (
+    <Container>Movie Details</Container>
+  );
+};
+
+export default MovieDetails;
